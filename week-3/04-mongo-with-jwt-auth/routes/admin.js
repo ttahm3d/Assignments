4e1053377c5f3b5db09a9faf510b993a1b3ce050
ttahm3d <ttahm3d@gmail.com>
2024-02-18 21:14:10 +0530

    [feat] - added partial soln for week3/04-jwt

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,49 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { JWT_SECRET } = require("../config");
 const router = Router();
+const jwt = require("jsonwebtoken");
+const { User } = require("../db/index");
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  const { username, password } = req.body;
+  await Admin.create({ username, password });
+  res.status(201).send({ message: "Admin created successfully" });
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post("/signin", async (req, res) => {
+  // Implement admin signup logic
+  const { username, password } = req.body;
+  if (!username || !password) res.status(401);
+  const user = User.findOne({ username, password });
+  if (user) {
+    const token = JWT.sign({ username }, JWT_SECRET);
+    res.send({ token });
+  } else {
+    res.status(411).send({ message: "Incorrect inputs" });
+  }
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, async (req, res) => {
+  // Implement course creation logic
+  const { title, description, price, imageLink } = req.body;
+  const newCourse = await Course.create({
+    title,
+    description,
+    price,
+    imageLink,
+  });
+  res.send({ message: "Course created successfully", courseId: newCourse._id });
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, async (req, res) => {
+  // Implement fetching all courses logic
+  const courses = await Course.find({});
+  res.send({
+    courses,
+  });
 });
 
 module.exports = router;
--- week-3/04-mongo-with-jwt-auth/solution/db/index.js
+++ week-3/04-mongo-with-jwt-auth/solution/db/index.js
@@ -1,39 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/course_selling_app2');
+// // Connect to MongoDB
+// mongoose.connect("mongodb://localhost:27017/coursesellingapp", () =>
+//   console.log("connected to db")
+// );
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
-    username: String,
-    password: String
+  // Schema definition here
+  username: String,
+  password: String,
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
-    username: String,
-    password: String,
-    purchasedCourses: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Course'
-    }]
+  // Schema definition here
+  username: String,
+  password: String,
+  courses: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+  },
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
-    title: String,
-    description: String,
-    imageLink: String,
-    price: Number
+  // Schema definition here
+  title: String,
+  description: String,
+  imageLink: String,
+  price: Number,
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
-}
+  Admin,
+  User,
+  Course,
+};
