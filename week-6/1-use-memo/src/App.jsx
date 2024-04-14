1b6b2c60d8517cee9be60f6b35f37b6a2d8e5a05
ttahm3d <ttahm3d@gmail.com>
2024-04-14 18:20:51 +0530

    [feat] - added solution for week 6

--- week-6/1-use-memo/src/App.jsx
+++ week-6/1-use-memo/src/App.jsx
@@ -1,7 +1,8 @@
-import './App.css'
-import { Assignment1 } from './components/Assignment1'
-import { Assignment2 } from './components/Assignment2'
-import { Assignment3 } from './components/Assignment3'
+/* eslint-disable no-unused-vars */
+import "./App.css";
+import { Assignment1 } from "./components/Assignment1";
+import { Assignment2 } from "./components/Assignment2";
+import { Assignment3 } from "./components/Assignment3";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       {/* <Assignment2 /> */}
       {/* <Assignment3 /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
